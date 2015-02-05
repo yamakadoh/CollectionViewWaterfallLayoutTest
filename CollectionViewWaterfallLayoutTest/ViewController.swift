1b3099055a04d999b79ff9be7b37e2e713b1bb54
yamakado <yamakado@basicinc.jp>
2015-02-05 22:49:12 +0900

    独自CollectionViewCellを実装(途中)

--- CollectionViewWaterfallLayoutTest/ViewController.swift
+++ CollectionViewWaterfallLayoutTest/ViewController.swift
@@ -38,6 +38,47 @@ class MyCollectionViewFooter: UICollectionReusableView {
 }
 
 class MyCollectionViewCell: UICollectionViewCell {
+    var thumbnail = UIImageView()
+    var title = UILabel()
+    var source = UILabel()
+    var category = UIImageView()
+    
+    var number = UILabel()  // TODO: デバッグ用。あとで消す
+    
+    override init(frame: CGRect) {
+        println("[MyCollectionViewCell]init called")
+        super.init(frame: frame)
+        backgroundColor = UIColor.whiteColor()
+        
+        // サムネイル
+        let rectThumbnail = CGRect(x: 3, y: 3, width: frame.size.width - 3*2, height: 0)
+        thumbnail.frame = rectThumbnail
+        contentView.addSubview(thumbnail)
+        
+        // タイトル
+        contentView.addSubview(title)
+        
+        // ソース
+        contentView.addSubview(source)
+        
+        // カテゴリ
+        contentView.addSubview(category)
+        
+        // セル番号
+        let rectNumber = CGRect(x: 0, y: 0, width: frame.size.width, height: UIFont.smallSystemFontSize())
+        //println("frame = \(frame)")
+        //println("textFrame = \(rectNumber)")
+        number.frame = rectNumber
+        number.textAlignment = NSTextAlignment.Left
+        number.font = UIFont.systemFontOfSize(UIFont.smallSystemFontSize())
+        number.textColor = UIColor.blackColor()
+        contentView.addSubview(number)
+    }
+
+    required init(coder aDecoder: NSCoder) {
+        //fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+    }
 }
 
 class ViewController: UIViewController, UICollectionViewDataSource, CollectionViewWaterfallLayoutDelegate, UIGestureRecognizerDelegate {
@@ -48,10 +89,12 @@ class ViewController: UIViewController, UICollectionViewDataSource, CollectionVi
     lazy var cellSizes: [CGSize] = {
         var _cellSizes = [CGSize]()
         
-        for _ in 0...10 {
-            let random = Int(arc4random_uniform((UInt32(100))))
-            
-            _cellSizes.append(CGSize(width: 140, height: 50 + random))
+        for i in 0...13 {
+            //let random = Int(arc4random_uniform((UInt32(100))))
+            let image = UIImage(named: "\(i).jpg")!
+            let widthCell: CGFloat = 140
+            let heightCell: CGFloat = widthCell * image.size.height / image.size.width
+            _cellSizes.append(CGSize(width: widthCell, height: heightCell + 80))
         }
         
         return _cellSizes
@@ -61,19 +104,22 @@ class ViewController: UIViewController, UICollectionViewDataSource, CollectionVi
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         println("[ViewController]viewDidLoad")
+        self.view?.backgroundColor = UIColor.whiteColor()
         
         let layout = CollectionViewWaterfallLayout()
         layout.sectionInset = UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)    // コンテンツ部のマージン
         layout.headerInset = UIEdgeInsets(top: /*20*/0, left: 0, bottom: 0, right: 0)   // ヘッダ部のマージン(20はステータスバーの高さ)
-        layout.headerHeight = 170
-        layout.footerHeight = 50
+        let image = UIImage(named: "top1.png")!
+        layout.headerHeight = Float(UIScreen.mainScreen().bounds.width * image.size.height / image.size.width)
+        layout.footerHeight = 0
         layout.minimumColumnSpacing = 10
         layout.minimumInteritemSpacing = 10
         
-        //collectionView.backgroundColor = UIColor.blackColor()
+        collectionView.backgroundColor = UIColor.lightGrayColor()
         collectionView.collectionViewLayout = layout
         collectionView.registerClass(/*UICollectionReusableView.self*/MyCollectionViewHeader.self, forSupplementaryViewOfKind: CollectionViewWaterfallElementKindSectionHeader, withReuseIdentifier: "Header")
         collectionView.registerClass(UICollectionReusableView.self, forSupplementaryViewOfKind: CollectionViewWaterfallElementKindSectionFooter, withReuseIdentifier: "Footer")
+        collectionView.registerClass(MyCollectionViewCell.self, forCellWithReuseIdentifier: "Cell")
         
         // ジェスチャー
         // タップ
@@ -99,11 +145,14 @@ class ViewController: UIViewController, UICollectionViewDataSource, CollectionVi
     }
     
     func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+        println("[cellForItemAtIndexPath indexPath:\(indexPath.row)]called")
         
-        if let label = cell.contentView.viewWithTag(1) as? UILabel {
-            label.text = String(indexPath.row)
-        }
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as MyCollectionViewCell
+        cell.number.text = String(indexPath.row)
+        let thumbnail = UIImage(named: "\(indexPath.row).jpg")
+        cell.thumbnail.image = thumbnail
+        cell.thumbnail.frame.size = CGSize(width: cell.thumbnail.frame.size.width, height: cell.thumbnail.frame.width * thumbnail!.size.height / thumbnail!.size.width)
+        println("cell.thumbnail.frame.size = \(cell.thumbnail.frame.size)")
         
         return cell
     }
@@ -113,10 +162,6 @@ class ViewController: UIViewController, UICollectionViewDataSource, CollectionVi
         
         if kind == CollectionViewWaterfallElementKindSectionHeader {
             reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "Header", forIndexPath: indexPath) as? /*UICollectionReusableView*/MyCollectionViewHeader
-            
-//            if let view = reusableView {
-//                view.backgroundColor = UIColor.redColor()
-//            }
         }
         else if kind == CollectionViewWaterfallElementKindSectionFooter {
             reusableView = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "Footer", forIndexPath: indexPath) as? UICollectionReusableView
@@ -131,7 +176,7 @@ class ViewController: UIViewController, UICollectionViewDataSource, CollectionVi
     // MARK: WaterfallLayoutDelegate
     
     func collectionView(collectionView: UICollectionView, layout: UICollectionViewLayout, sizeForItemAtIndexPath indexPath: NSIndexPath) -> CGSize {
-        println("cellSizes[indexPath.item]=\(cellSizes[indexPath.item])")
+        println("[sizeForItemAtIndexPath]cellSizes[\(indexPath.item)]=\(cellSizes[indexPath.item])")
         return cellSizes[indexPath.item]
     }
 
